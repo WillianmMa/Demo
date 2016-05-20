64992eb187f515796c8761a40ace7343f4b96a96
adminMa <374393247@qq.com>
2016-05-20 10:40:39 +0800

    测试

--- MyApplication/app/src/main/java/com/runcam/picpass/myapplication/LoginActivity.java
+++ MyApplication/app/src/main/java/com/runcam/picpass/myapplication/LoginActivity.java
@@ -35,10 +35,6 @@ import java.util.List;
 import static android.Manifest.permission.READ_CONTACTS;
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
     private static final int REQUEST_READ_CONTACTS = 0;
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
     };
